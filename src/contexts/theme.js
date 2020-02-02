f5990860097e03ec56a35a306286af613f4c97fa
Dan Billson <danny.billson@hotmail.com>
2020-02-02 19:29:48 +0000

    temp rerender on load

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import { useStateValue } from "./state"
 import { themes } from "../config/themes"
@@ -6,6 +6,8 @@ import { themes } from "../config/themes"
 export const AppThemeProvider = ({ children }) => {
   const [theme] = useStateValue()
   console.log({ theme })
+
+  useEffect(() => {}, [theme])
 
   return <ThemeProvider theme={themes[theme]}>{children}</ThemeProvider>
 }
