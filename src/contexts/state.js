4ec4645478a7731efd74afeca9da1c8b03b1ed24
Dan Billson <danny.billson@hotmail.com>
2020-02-02 18:45:12 +0000

    check for window properly

--- src/contexts/state.js
+++ src/contexts/state.js
@@ -1,10 +1,11 @@
 import React, { createContext, useContext } from "react"
 import createPersistedState from "use-persisted-state"
 
-const usePersistedState = createPersistedState(
-  "state",
-  window && window.sessionStorage
-)
+const sessionStorage = () => {
+  if (typeof window !== undefined) return window.sessionStorage
+}
+
+const usePersistedState = createPersistedState("state", sessionStorage())
 
 export const StateContext = createContext()
 
