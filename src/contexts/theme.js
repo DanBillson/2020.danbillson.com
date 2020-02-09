82b8cb92276f632c43d0900512c6907b4474ce1a
Dan Billson <danny.billson@hotmail.com>
2020-02-09 12:09:43 +0000

    try use layout effect

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useLayoutEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import { useStateValue } from "./state"
 import { themes } from "../config/themes"
 
 export const AppThemeProvider = ({ children }) => {
   const [theme] = useStateValue()
+
+  useLayoutEffect(() => {}, [theme])
 
   return <ThemeProvider theme={themes[theme]}>{children}</ThemeProvider>
 }
